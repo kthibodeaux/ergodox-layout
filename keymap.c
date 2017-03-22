73b4b3714d8666b4db88f8f4ca0474d30d27d85c
Keith Thibodeaux <keith@railscoder.net>
2017-03-22 11:32:13 -0400

    Use a OSM for shifting single letters

--- keymap.c
+++ keymap.c
@@ -48,13 +48,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,--------------------------------------------------.           ,--------------------------------------------------.
    * |   =    |   1  |   2  |   3  |   4  |   5  | GAME |           | GAME |   6  |   7  |   8  |   9  |   0  |   -    |
    * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
-   * | Del    |   Q  |   W  |   F  |   P  |   G  |      |           |      |   J  |   L  |   U  |   Y  |   ;  |   \    |
+   * | Del    |   Q  |   W  |   F  |   P  |   G  |      |           |      |   J  |   L  |   U  |   Y  |LShift|   \    |
    * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
    * | BkSp   |   A  |  a/R |   S  |  ^/T |   D  |------|           |------|   H  |  ^/N |  t/E |  a/I |   O  |' / Cmd |
    * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
    * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   K  |   M  |   ,  |   .  |   /  | RShift |
    * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
-   *   | DYN  |      |      | LSFT | SYMB |                                       | SYMB | LSFT |      |      | DYN  |
+   *   | DYN  |      |      |      | SYMB |                                       | SYMB |      |      |      | DYN  |
    *   `----------------------------------'                                       `----------------------------------'
    *                                        ,-------------.       ,-------------.
    *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -72,16 +72,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_DELT,          KC_Q,         KC_W,   KC_F,   KC_P,   KC_G,   KC_NO,
       KC_BSPC,          KC_A,         ALT_T(KC_R),   KC_S,   CTL_T(KC_T),   KC_D,
       KC_LSFT,          KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-      MO(_DYN), KC_TRNS, KC_TRNS, KC_LSFT, MO(SYMB),
+      MO(_DYN), KC_TRNS, KC_TRNS, KC_NO, MO(SYMB),
       ALT_T(KC_APP),    KC_LGUI,
       KC_HOME,
       TD(TD_SPC),           KC_BSPC,     CTL_T(KC_ESC),
       // right hand
       TG(GAME),         KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-      KC_NO,            KC_J,   KC_L,  KC_U,   KC_Y,   KC_SCLN,             KC_BSLS,
+      KC_NO,            KC_J,   KC_L,  KC_U,   KC_Y,   OSM(MOD_LSFT),             KC_BSLS,
       KC_H,             CTL_T(KC_N),    LT(TMUX, KC_E),   ALT_T(KC_I),   KC_O,          GUI_T(KC_QUOT),
       MEH_T(KC_NO),     KC_K,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,
-      MO(SYMB), KC_LSFT, KC_TRNS, KC_TRNS, MO(_DYN),
+      MO(SYMB), KC_NO, KC_TRNS, KC_TRNS, MO(_DYN),
       KC_LALT,          CTL_T(KC_ESC),
       KC_PGUP,
       KC_PGDN,          KC_TAB, KC_ENT
@@ -132,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,--------------------------------------------------.           ,--------------------------------------------------.
    * |        |  F1  |  F2  |  F3  |  F4  |  F5  |  F11 |           |  F12 |  F6  |  F7  |  F8  |  F9  |  F10 |        |
    * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
-   * |        |      |   ~  |   {  |   }  |      |      |           |      | MUTE |   (  |   )  |   `  |      |        |
+   * |        |      |   ~  |   {  |   }  |      |      |           |      | MUTE |   (  |   )  |   `  |  ;   |        |
    * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
    * |        |   &  |   |  |   '  |   "  |      |------|           |------| VOLU |   :  |   @  |   -  |  #   |        |
    * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -160,7 +160,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LEFT, KC_RIGHT, KC_TRNS,
       // right hand
       KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
-      KC_TRNS, KC_MUTE, KC_LPRN, KC_RPRN, KC_GRV,  KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_MUTE, KC_LPRN, KC_RPRN, KC_GRV,  KC_SCLN, KC_TRNS,
                KC_VOLU, KC_COLN, KC_AT,   KC_MINS, KC_HASH, KC_TRNS,
       KC_TRNS, KC_VOLD, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS,
                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
